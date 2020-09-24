08965e72e49198c45496f0ed308b06bd35e4aeb3
Marija Marin <marijamarin2508@gmail.com>
2020-09-24 17:54:03 +0200

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getDayFoto, spaceNews } from './services';
-import { BrowserRouter, Route, Link, Redirect, Switch } from 'react-router-dom';
+import { HashRouter, Route, Link, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import NasaPhoto from './NasaPhoto';
@@ -17,7 +17,7 @@ import Mars from './Mars';
 const App = ({ users }) => {
 	const [ user, setUser ] = useState(null);
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter basename={process.env.PUBLIC_URL}>
 			<div
 				className="main-div"
 				style={{
@@ -40,7 +40,7 @@ const App = ({ users }) => {
 				<Route component={Mars} path="/mars" />
 				<Route path="/contact">{user ? <Contact /> : <Redirect to="/signin" />}</Route>
 			</div>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
 
